ccfbd2c6ea6bdec29a3cb86452dd25393fba5bca
hojiakbar2806 <hojiakbarnasriddinov2006@gmail.com>
2025-06-01 14:11:04 +0500

    fixed

--- backend/app/utils/send_password.py
+++ backend/app/utils/send_password.py
@@ -9,7 +9,11 @@ def send_password(email: str, password: str):
     msg["Subject"] = "Hisob yaratildi"
     msg["From"] = settings.EMAIL_HOST_USER
     msg["To"] = email
-    msg.set_content(f"Sizning parolingiz: {password}")
+    msg.set_content(
+        f"""
+        Sizning foydalanuvchi nomingiz: {email}
+        Sizning parolingiz: {password}
+        """)
     with SMTP(host="smtp.gmail.com", port=587) as smtp:
         smtp.ehlo()
         smtp.starttls()
